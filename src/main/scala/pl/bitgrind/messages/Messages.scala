eb2232a8165d10e8be7ba154aad5133310c24866
oegnus <tomasz.wiencek@gmail.com>
2015-08-10 13:44:41 +0200

    fixed db column names

--- src/main/scala/pl/bitgrind/messages/Messages.scala
+++ src/main/scala/pl/bitgrind/messages/Messages.scala
@@ -104,8 +104,8 @@ trait MessagesDb {
 
   class MessagesTable(tag: Tag) extends Table[Message](tag, "MESSAGES") {
     def id = column[MessageId]("MSG_ID", O.PrimaryKey, O.AutoInc)
-    def toUser = column[UserId]("FROM_USER")
-    def fromUser = column[UserId]("TO_USER")
+    def toUser = column[UserId]("TO_USER")
+    def fromUser = column[UserId]("FROM_USER")
     def content = column[String]("CONTENT")
     def * = (id, toUser, fromUser, content) <>(Message.tupled, Message.unapply)
   }
